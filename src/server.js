01019ce39447678e22057fabeae9448e181bf49c
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-04-20 18:56:47 +0300

    create file upload

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import express from 'express';
-import fileUpload from 'express-fileupload';
+import multipart from 'connect-multiparty';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import expressJwt from 'express-jwt';
@@ -8,6 +8,9 @@ import jwt from 'jsonwebtoken';
 import PrettyError from 'pretty-error';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import mv from 'mv';
+import del from 'del';
+import shortid from 'shortid';
 
 import Comment from './models/comment';
 import Hashtag from './models/hashtag';
@@ -15,137 +18,107 @@ import Image from './models/image';
 import Like from './models/like';
 import Mention from './models/mention';
 import User from './models/user';
+import {
+  port,
+  auth,
+  debug,
+  ip,
+} from './config';
 
-/*let hashtag = new Hashtag({
-  text: 'helloworld'
-});
-hashtag.save();*/
-
-
-import { port, auth, debug, ip, } from './config';
+// ========================================================
 const app = express();
-
+const router = express.Router();
+const multipartMiddleware = multipart();
 mongoose.connect(`mongodb://localhost/imghurdur`);
 
+// ========================================================
 // register server middleware
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'mediaroot')));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
-app.use(fileUpload());
+
+// ========================================================
 // error handling
 const pe = new PrettyError();
 pe.skipNodeFiles();
 pe.skipPackage('express');
 
-/*app.use((err, req, res, next) => {
-  console.log(pe.render(err));
-  const template = require('./views/error.jade');
-  const statusCode = err.status || 500;
-  res.status(statusCode);
-  res.send(template({
-    message: err.message,
-    stack: debug ? err.stack : '',
-  }));
-});*/
-
-const router = express.Router();
+// ========================================================
 
 router.route('/test')
   .get((req, res) => {
-    res.json({hello: 'world!'});
+    res.json({
+      hello: 'world!'
+    });
   });
 
 router.route('/hashtag')
   .get((req, res) => {
     Hashtag.find((err, hashtags) => {
-      if (err) { res.send(err); }
+      if (err) {
+        res.send(err);
+      }
       res.send(hashtags);
     });
   });
+
 app.use('/api', router);
 
-app.post('/upload', (req, res) => {
-  let file;
-  if (!req.files) {
-    res.send('no files were uploaded');
+app.post('/upload', multipartMiddleware, (req, res) => {
+  // TODO check types, image size, filename, csrf
+  let filename = req.files.file.name;
+  if (!filename.match(/([a-zA-Z0-9\.\-\_]{1,20})(\.)(png|jp(e)?g|gif)$/)) {
+    res.status(403)
+      .json({
+        success: false,
+        message: 'invalid file'
+      });
     return;
   }
-  file = req.files.file;
-  file.mv(path.join(__dirname, '/mediaroot/uploads'), (err) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send('file uploaded!');
-    }
-  })
-});
 
-/*app.get('/test', (req, res) => {
-  res.render(``);
-})*/
+  const extension = filename.split(/(png|jp(e)?g|gif)$/)[1];
+  console.log(`extension: '${extension}'`);
+  console.log(`filename: '${filename}'`);
+  filename = filename.substr(0, filename.length  - 1 - extension.length);
+  console.log(`filename: '${filename}'`);
+  filename += '_';
+  filename += shortid.generate();
+  console.log(`filename: '${filename}'`);
+  filename += '.';
+  filename += extension;
+  console.log(`filename: '${filename}'`);
+  mv(req.files.file.path,
+    path.join(__dirname, '/mediaroot/uploads/' + filename), {
+      mkdirp: true
+    }, (err) => {
+      if (err) {
+        res.send(err);
+      }
+    });
+
+  let img = new Image({
+    title: 'Cool pic',
+    description: 'Foo bar baz',
+    uploadPath: filename
+  });
+
+  del([req.files.file.path])
+    .then(paths => {
+      console.log('Deleted files and folders:\n',
+        paths.join('\n'));
+    });
+  req.files = null;
+
+  img.save();
+  res.json(img);
+
+});
 
 console.log('Server started!');
 console.log(`Listening on ${ip}:${port}`);
 app.listen(port);
-
-// authentication
-/*app.use(expressJwt({
-  secret: auth.jwt.secret,
-  credentialsRequired: false,
-  getToken: req => req.cookies.id_token,
-}));*/
-
-// register API middleware
-/*app.use('/graphql', expressGraphQL(req => ({
-  schema,
-  graphiql: true,
-  rootValue: { request: req },
-  pretty: debug,
-})));*/
-
-
-
-// launch server
-/*models.sync().catch(err => console.error(err.stack)).then(() => {
-  app.listen(port, () => {
-    console.log(`The server is running at http://localhost:${port}/`);
-  })
-});*/
-
-//
-// const PORT = process.env.PORT || 8000;
-// const IP = process.env.IP || 'localhost';
-// const DEBUG = process.env.NODE_ENV !== 'production';
-
-// const schema = new GraphQLSchema({
-//   query: new GraphQLObjectType({
-//     name: 'RootQueryType',
-//     fields: {
-//       hello: {
-//         type: GraphQLString,
-//         resolve() {
-//           return 'world';
-//         }
-//       }
-//     }
-//   })
-// });
-//
-// const apiRouter = new express.Router();
-//
-// apiRouter.get('/', (req, res) => {
-//   res.send('welcome to api!');
-// });
-//
-// app.use('/api', apiRouter);
-//
-// app.use('/graphql', graphqlHTTP({
-//   schema: schema,
-//   graphiql: DEBUG
-// }));
-
-/*app.get('/', (req, res) => {
-  res.json({hello: 'world!'});
-});*/
