8c3b1fd90303301d372cf44345ea723d801b2a40
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-04-20 21:00:20 +0300

    add some dank middleware to server

--- src/server.js
+++ src/server.js
@@ -1,23 +1,34 @@
+// Utils
+import mv from 'mv';
+import del from 'del';
 import path from 'path';
+import shortid from 'shortid';
+
+// Express and express middleware
 import express from 'express';
 import multipart from 'connect-multiparty';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import expressJwt from 'express-jwt';
-import jwt from 'jsonwebtoken';
 import PrettyError from 'pretty-error';
 import morgan from 'morgan';
-import mongoose from 'mongoose';
-import mv from 'mv';
-import del from 'del';
-import shortid from 'shortid';
+//import admin from 'sriracha-admin';
+import favicon from 'serve-favicon';
+//import expressDebug from 'express-debug';
+import timeout from 'connect-timeout';
+import compression from 'compression';
 
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+// Models
 import Comment from './models/comment';
 import Hashtag from './models/hashtag';
 import Image from './models/image';
 import Like from './models/like';
 import Mention from './models/mention';
 import User from './models/user';
+// Configs
 import {
   port,
   auth,
@@ -33,29 +44,68 @@ mongoose.connect(`mongodb://localhost/imghurdur`);
 
 // ========================================================
 // register server middleware
+app.use(timeout('5s'));
 app.use(morgan('combined'));
+app.use(haltOnTimedout);
+app.use(compression());
+app.use(haltOnTimedout);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'mediaroot')));
 app.use(cookieParser());
+app.use(haltOnTimedout);
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(haltOnTimedout);
 app.use(bodyParser.json());
+app.use(haltOnTimedout);
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
+app.use(haltOnTimedout);
+//app.use('/admin', admin());
+//app.use(haltOnTimedout);
+//expressDebug(app, {});
+//app.use(haltOnTimedout);
 
 // ========================================================
 // error handling
 const pe = new PrettyError();
 pe.skipNodeFiles();
 pe.skipPackage('express');
+pe.start();
 
-// ========================================================
 
-router.route('/test')
-  .get((req, res) => {
-    res.json({
-      hello: 'world!'
+// Set to print output html from jade prettily
+// https://stackoverflow.com/a/11812841
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
+
+// Error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
   });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+// ========================================================
 
 router.route('/hashtag')
   .get((req, res) => {
@@ -66,6 +116,19 @@ router.route('/hashtag')
       res.send(hashtags);
     });
   });
+
+router.route('/image')
+  .get((req, res) => {
+    Image.find((err, images) => {
+      if (err) {
+        res.status(500)
+          .send(err);
+      } else {
+        res.send(images);
+      }
+    });
+  })
+  .post();
 
 app.use('/api', router);
 
@@ -82,16 +145,10 @@ app.post('/upload', multipartMiddleware, (req, res) => {
   }
 
   const extension = filename.split(/(png|jp(e)?g|gif)$/)[1];
-  console.log(`extension: '${extension}'`);
-  console.log(`filename: '${filename}'`);
-  filename = filename.substr(0, filename.length  - 1 - extension.length);
-  console.log(`filename: '${filename}'`);
-  filename += '_';
-  filename += shortid.generate();
-  console.log(`filename: '${filename}'`);
-  filename += '.';
-  filename += extension;
-  console.log(`filename: '${filename}'`);
+  filename =
+    `${filename.substr(0, filename.length  - 1 - extension.length)}` +
+    `_${shortid.generate()}.${extension}`;
+
   mv(req.files.file.path,
     path.join(__dirname, '/mediaroot/uploads/' + filename), {
       mkdirp: true
@@ -118,6 +175,10 @@ app.post('/upload', multipartMiddleware, (req, res) => {
   res.json(img);
 
 });
+
+function haltOnTimedout(req, res, next) {
+  if (!req.timedout) next();
+}
 
 console.log('Server started!');
 console.log(`Listening on ${ip}:${port}`);
